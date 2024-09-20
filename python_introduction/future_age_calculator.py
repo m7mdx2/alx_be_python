fa857bdd866e73e8e8914f49552d27176cfef062
m7mdx2 <mo.a7med15@gmail.com>
2024-09-20 08:06:53 +0300

    Add python_introduction directory with new files

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+current_age = int(input("How old are you? "))
+
+future_year = 2050
+current_year = 2023
+age_in_2050 = current_age + (future_year - current_year)
+
+print(f"In {future_year}, you will be {age_in_2050} years old.")
